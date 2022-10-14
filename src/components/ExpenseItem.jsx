b3808c7b17e5397cb09027e8dd960e2b3df77dcd
hashvector <ibrahimabdulqoyyum@gmail.com>
2022-10-14 21:41:40 +0100

    Passing Data via props

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,16 +1,12 @@
 import './ExpenseItem.css';
 
 function ExpenseItem() {
-  const expenseDate = new Date(2021, 2, 28);
-  const expenseItem = 'Car insurance';
-  const expenseAmount = 294.67;
-
   return (
     <div className="expense-item">
-      <div>{expenseDate.toISOString()}</div>
+      <div>March 28, 2021</div>
       <div className="expense-item__description">
-        <h2>{expenseItem}</h2>
-        <div className="expense-item__price">${expenseAmount}</div>
+        <h2>Car insurance</h2>
+        <div className="expense-item__price">$294.67</div>
       </div>
     </div>
   );
