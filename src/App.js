b73ddb6b238bd05ba6d9a2ddd141b5f62fcda7d7
hashvector <ibrahimabdulqoyyum@gmail.com>
2022-10-14 21:41:36 +0100

    Passing Data via props

--- src/App.js
+++ src/App.js
@@ -4,7 +4,16 @@ function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem />
+      <ExpenseItem
+        amount="239.67"
+        title="Car insurance"
+        date={new Date(2021, 2, 28)}
+      />
+      <ExpenseItem
+        amount="340.23"
+        title="House rent"
+        date={new Date(2019, 4, 15)}
+      />
     </div>
   );
 }
