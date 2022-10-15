7f57495b38fd021c29f1ed6d2d368fdbbe28f925
hashvector <ibrahimabdulqoyyum@gmail.com>
2022-10-15 02:48:59 +0100

    Adding normal JavaScript Logic to Components

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,9 +1,23 @@
 import './ExpenseItem.css';
 
 function ExpenseItem(props) {
+  const month = new Intl.DateTimeFormat(navigator.language, {
+    month: 'long',
+  }).format(props.date);
+
+  const year = props.date.getFullYear();
+
+  const day = new Intl.DateTimeFormat(navigator.language, {
+    day: '2-digit',
+  }).format(props.date);
+
   return (
     <div className="expense-item">
-      <div>{props.date.toISOString()}</div>
+      <div>
+        <div>{month}</div>
+        <div>{year}</div>
+        <div>{day}</div>
+      </div>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
