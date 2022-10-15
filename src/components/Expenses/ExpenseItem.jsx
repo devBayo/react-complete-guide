762f63cc65b0ad03052d6e84c806ce331b7427dd
hashvector <ibrahimabdulqoyyum@gmail.com>
2022-10-15 11:08:56 +0100

    working with state

--- src/components/Expenses/ExpenseItem.jsx
+++ src/components/Expenses/ExpenseItem.jsx
@@ -6,6 +6,8 @@ import ExpenseDate from './ExpenseDate';
 const ExpenseItem = props => {
   const [title, setTitle] = useState(props.title);
 
+  console.log('ExpenseItem evaluated by React');
+
   const clickHandler = () => {
     setTitle('Updated');
     console.log(title);
