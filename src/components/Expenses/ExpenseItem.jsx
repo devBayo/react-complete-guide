49a34d5f883f9f8dd3e629aa793b4b2c9eabf823
hashvector <ibrahimabdulqoyyum@gmail.com>
2022-10-15 10:41:39 +0100

    listening to events and working with event handlers

--- src/components/Expenses/ExpenseItem.jsx
+++ src/components/Expenses/ExpenseItem.jsx
@@ -3,6 +3,10 @@ import Card from '../UI/Card';
 import ExpenseDate from './ExpenseDate';
 
 const ExpenseItem = props => {
+  const clickHandler = () => {
+    console.log('Clicked');
+  };
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
@@ -10,6 +14,7 @@ const ExpenseItem = props => {
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 };
