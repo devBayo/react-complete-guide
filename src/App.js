b30b0204327e56157d45892275da47d8d526a0ca
hashvector <ibrahimabdulqoyyum@gmail.com>
2022-10-15 10:07:55 +0100

    a closer look at jsx

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
+import React from 'react';
+
 import Expenses from './components/Expenses';
 
 function App() {
+  // return React.createElement(
+  //   'div',
+  //   {},
+  //   React.createElement('h2', {}, "Let's get started!"),
+  //   React.createElement(Expenses, {})
+  // );
+
   return (
     <div>
       <h2>Let's get started!</h2>
