1a1a70e0dd725ff0cf76d393d4da1a5e7d7d9b83
hashvector <ibrahimabdulqoyyum@gmail.com>
2022-10-15 11:00:13 +0100

    working with state

--- src/components/Expenses/ExpenseItem.jsx
+++ src/components/Expenses/ExpenseItem.jsx
@@ -1,17 +1,21 @@
+import { useState } from 'react';
 import './ExpenseItem.css';
 import Card from '../UI/Card';
 import ExpenseDate from './ExpenseDate';
 
 const ExpenseItem = props => {
+  const [title, setTitle] = useState(props.title);
+
   const clickHandler = () => {
-    console.log('Clicked');
+    setTitle('Updated');
+    console.log(title);
   };
 
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
       <button onClick={clickHandler}>Change Title</button>
