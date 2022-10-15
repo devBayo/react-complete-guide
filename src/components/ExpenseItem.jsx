38c596e06a926c5a12bdb848dbc0643fa1adc26c
hashvector <ibrahimabdulqoyyum@gmail.com>
2022-10-15 02:54:19 +0100

    splitting components into multiple components

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,23 +1,10 @@
 import './ExpenseItem.css';
+import ExpenseDate from './ExpenseDate';
 
 function ExpenseItem(props) {
-  const month = new Intl.DateTimeFormat(navigator.language, {
-    month: 'long',
-  }).format(props.date);
-
-  const year = props.date.getFullYear();
-
-  const day = new Intl.DateTimeFormat(navigator.language, {
-    day: '2-digit',
-  }).format(props.date);
-
   return (
     <div className="expense-item">
-      <div>
-        <div>{month}</div>
-        <div>{year}</div>
-        <div>{day}</div>
-      </div>
+      <ExpenseDate date={props.date}/>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
