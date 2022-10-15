ef574dc1c05765ce6875809d384aa122729cf617
hashvector <ibrahimabdulqoyyum@gmail.com>
2022-10-15 10:15:20 +0100

    organizing component files

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import Expenses from './components/Expenses';
+import Expenses from './components/Expenses/Expenses';
 
 function App() {
   // return React.createElement(
