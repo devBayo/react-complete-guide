99433b5498a8f16d3ec2a9ae63506d0af2390e2a
hashvector <ibrahimabdulqoyyum@gmail.com>
2022-10-15 10:18:57 +0100

    switched to arrow function

--- src/components/Expenses/ExpenseItem.jsx
+++ src/components/Expenses/ExpenseItem.jsx
@@ -2,7 +2,7 @@ import './ExpenseItem.css';
 import Card from '../UI/Card';
 import ExpenseDate from './ExpenseDate';
 
-function ExpenseItem(props) {
+const ExpenseItem = props => {
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
@@ -12,6 +12,6 @@ function ExpenseItem(props) {
       </div>
     </Card>
   );
-}
+};
 
 export default ExpenseItem;
