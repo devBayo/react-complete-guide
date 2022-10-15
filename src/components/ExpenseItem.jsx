8c6151b8440554425ba1fecf16c2a9d2f912241b
hashvector <ibrahimabdulqoyyum@gmail.com>
2022-10-15 09:47:09 +0100

    The concept of composition (children props)

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,15 +1,16 @@
 import './ExpenseItem.css';
+import Card from './Card';
 import ExpenseDate from './ExpenseDate';
 
 function ExpenseItem(props) {
   return (
-    <div className="expense-item">
-      <ExpenseDate date={props.date}/>
+    <Card className="expense-item">
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-    </div>
+    </Card>
   );
 }
 
