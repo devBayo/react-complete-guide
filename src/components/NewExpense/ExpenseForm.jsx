3204f6d2577df38d3b98a6364aeffb3e83a0899e
hashvector <ibrahimabdulqoyyum@gmail.com>
2022-10-16 08:58:56 +0100

    using one state instead

--- src/components/NewExpense/ExpenseForm.jsx
+++ src/components/NewExpense/ExpenseForm.jsx
@@ -2,22 +2,30 @@ import { useState } from 'react';
 import './ExpenseForm.css';
 
 const ExpenseForm = () => {
-  const [enteredTitle, setEnteredTitle] = useState('');
-  const [enteredAmount, setEnteredAmount] = useState('');
-  const [enteredDate, setEnteredDate] = useState('');
+  const [userInput, setUserInput] = useState({
+    enteredTitle: '',
+    enteredAmount: '',
+    enteredDate: '',
+  });
+  // const [enteredAmount, setEnteredAmount] = useState('');
+  // const [enteredDate, setEnteredDate] = useState('');
 
   const titleChangeHandler = event => {
-    setEnteredTitle(event.target.value);
+    // setEnteredTitle(event.target.value);
+    setUserInput({
+      ...userInput,
+      enteredTitle: event.target.value,
+    });
     console.log(event.target.value);
   };
 
   const amountChangeHandler = event => {
-    setEnteredAmount(event.target.value);
+    // setEnteredAmount(event.target.value);
     console.log(event.target.value);
   };
 
   const dateChangeHandler = event => {
-    setEnteredDate(event.target.value);
+    // setEnteredDate(event.target.value);
     console.log(event.target.value);
   };
 
