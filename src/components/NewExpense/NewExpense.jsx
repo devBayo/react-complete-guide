04e0d1732f8ee6d75969729fdc7c9c435194fe74
hashvector <ibrahimabdulqoyyum@gmail.com>
2022-10-16 10:47:34 +0100

    child to parent component communication

--- src/components/NewExpense/NewExpense.jsx
+++ src/components/NewExpense/NewExpense.jsx
@@ -2,13 +2,14 @@ import './NewExpense.css';
 
 import ExpenseForm from './ExpenseForm';
 
-const NewExpense = () => {
+const NewExpense = props => {
   const saveExpenseDataHandler = enteredExpenseDate => {
     const expenseData = {
       ...enteredExpenseDate,
       id: Math.random().toString(),
     };
     console.log(expenseData);
+    props.onAddExpense(expenseData);
   };
 
   return (
