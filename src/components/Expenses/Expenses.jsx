678517bb0614cbd1d5c119e636d2105b3d98747d
hashvector <ibrahimabdulqoyyum@gmail.com>
2022-10-16 19:00:10 +0100

    changed expenses

--- src/components/Expenses/Expenses.jsx
+++ src/components/Expenses/Expenses.jsx
@@ -2,19 +2,28 @@ import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
 import './Expenses.css';
 
-const Expenses = () => {
+const Expenses = props => {
   return (
     <Card className="expenses">
-      <ExpenseItem amount="500.00" title="School fees" date={new Date()} />
       <ExpenseItem
-        amount="239.67"
-        title="Car insurance"
-        date={new Date(2021, 2, 28)}
+        amount={props.item[0].amount}
+        title={props.item[0].title}
+        date={props.item[0].date}
       />
       <ExpenseItem
-        amount="340.23"
-        title="House rent"
-        date={new Date(2019, 4, 15)}
+        amount={props.item[1].amount}
+        title={props.item[1].title}
+        date={props.item[1].date}
+      />
+      <ExpenseItem
+        amount={props.item[2].amount}
+        title={props.item[2].title}
+        date={props.item[2].date}
+      />
+      <ExpenseItem
+        amount={props.item[3].amount}
+        title={props.item[3].title}
+        date={props.item[3].date}
       />
     </Card>
   );
