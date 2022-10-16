9205cc9163071735e67219532de6ec1d46f68d47
hashvector <ibrahimabdulqoyyum@gmail.com>
2022-10-16 19:30:20 +0100

    adding expenses filter

--- src/components/Expenses/Expenses.jsx
+++ src/components/Expenses/Expenses.jsx
@@ -1,10 +1,23 @@
+import { useState } from 'react';
+
 import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
 import './Expenses.css';
+import ExpensesFilter from './ExpensesFilter';
 
 const Expenses = props => {
+  const [filteredYear, setFilteredYear] = useState('2020');
+
+  const filterChangedHandler = selectedYear => {
+    setFilteredYear(selectedYear);
+  };
+
   return (
     <Card className="expenses">
+      <ExpensesFilter
+        selected={filteredYear}
+        onChangeFilter={filterChangedHandler}
+      />
       <ExpenseItem
         amount={props.item[0].amount}
         title={props.item[0].title}
