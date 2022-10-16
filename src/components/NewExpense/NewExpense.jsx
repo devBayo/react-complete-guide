c160db6c87df78ba09183d1e237f335d9429b8f5
hashvector <ibrahimabdulqoyyum@gmail.com>
2022-10-16 10:41:21 +0100

    child to parent component communication

--- src/components/NewExpense/NewExpense.jsx
+++ src/components/NewExpense/NewExpense.jsx
@@ -3,9 +3,17 @@ import './NewExpense.css';
 import ExpenseForm from './ExpenseForm';
 
 const NewExpense = () => {
+  const saveExpenseDataHandler = enteredExpenseDate => {
+    const expenseData = {
+      ...enteredExpenseDate,
+      id: Math.random().toString(),
+    };
+    console.log(expenseData);
+  };
+
   return (
     <div className="new-expense">
-      <ExpenseForm />
+      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
     </div>
   );
 };
