3a1352c4cfefc1247a1d27d82c6fbb321a864bfe
hashvector <ibrahimabdulqoyyum@gmail.com>
2022-10-17 08:06:57 +0100

    rendering list of data

--- src/components/Expenses/Expenses.jsx
+++ src/components/Expenses/Expenses.jsx
@@ -18,26 +18,14 @@ const Expenses = props => {
         selected={filteredYear}
         onChangeFilter={filterChangedHandler}
       />
-      <ExpenseItem
-        amount={props.item[0].amount}
-        title={props.item[0].title}
-        date={props.item[0].date}
-      />
-      <ExpenseItem
-        amount={props.item[1].amount}
-        title={props.item[1].title}
-        date={props.item[1].date}
-      />
-      <ExpenseItem
-        amount={props.item[2].amount}
-        title={props.item[2].title}
-        date={props.item[2].date}
-      />
-      <ExpenseItem
-        amount={props.item[3].amount}
-        title={props.item[3].title}
-        date={props.item[3].date}
-      />
+      {props.item.map(expense => (
+        <ExpenseItem
+          key={Math.random()}
+          amount={expense.amount}
+          title={expense.title}
+          date={expense.date}
+        />
+      ))}
     </Card>
   );
 };
