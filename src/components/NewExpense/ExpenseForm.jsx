5d7500e526d9c279176939e19c0c79c2c2eb85ad
hashvector <ibrahimabdulqoyyum@gmail.com>
2022-10-17 11:35:52 +0100

    fixed bug.

--- src/components/NewExpense/ExpenseForm.jsx
+++ src/components/NewExpense/ExpenseForm.jsx
@@ -35,7 +35,7 @@ const ExpenseForm = props => {
 
     const expenseData = {
       title: enteredTitle,
-      amount: enteredAmount,
+      amount: +enteredAmount,
       date: new Date(enteredDate),
     };
 
