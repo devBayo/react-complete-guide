0ef8cedfbf54a8a32d69af47b4a774e7ba207831
hashvector <ibrahimabdulqoyyum@gmail.com>
2022-10-17 11:35:38 +0100

    added chart

--- src/components/Expenses/Expenses.jsx
+++ src/components/Expenses/Expenses.jsx
@@ -4,6 +4,7 @@ import ExpensesList from './ExpensesList';
 import Card from '../UI/Card';
 import './Expenses.css';
 import ExpensesFilter from './ExpensesFilter';
+import ExpensesChart from './ExpensesChart';
 
 const Expenses = props => {
   const [filteredYear, setFilteredYear] = useState('2020');
@@ -22,6 +23,7 @@ const Expenses = props => {
         selected={filteredYear}
         onChangeFilter={filterChangedHandler}
       />
+      <ExpensesChart expenses={filteredExpenses} />
       <ExpensesList items={filteredExpenses} />
     </Card>
   );
