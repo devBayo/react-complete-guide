dad104dc32b0750d14f3b75773725cf8d58fa1c1
hashvector <ibrahimabdulqoyyum@gmail.com>
2022-10-17 10:30:23 +0100

    modified expense form

--- src/components/NewExpense/NewExpense.jsx
+++ src/components/NewExpense/NewExpense.jsx
@@ -1,19 +1,39 @@
+import { useState } from 'react';
 import './NewExpense.css';
-
 import ExpenseForm from './ExpenseForm';
 
 const NewExpense = props => {
+  const [isEditing, setIsEditing] = useState(false);
+
   const saveExpenseDataHandler = enteredExpenseDate => {
     const expenseData = {
       ...enteredExpenseDate,
       id: Math.random().toString(),
     };
     props.onAddExpense(expenseData);
+    setIsEditing(false);
+  };
+
+  const startEditingHandler = () => {
+    setIsEditing(true);
+  };
+
+  const stopEditingHandler = () => {
+    setIsEditing(false);
   };
 
   return (
     <div className="new-expense">
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+      {!isEditing && (
+        <button onClick={startEditingHandler}>Add New Expense</button>
+      )}
+
+      {isEditing && (
+        <ExpenseForm
+          onSaveExpenseData={saveExpenseDataHandler}
+          onCancel={stopEditingHandler}
+        />
+      )}
     </div>
   );
 };
