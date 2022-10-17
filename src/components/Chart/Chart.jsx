e6a8fccf6922c9dcfc0d05bbaa963aa47150ef4b
hashvector <ibrahimabdulqoyyum@gmail.com>
2022-10-17 10:42:49 +0100

    adding a chart

---
+++ src/components/Chart/Chart.jsx
@@ -0,0 +1,19 @@
+import './Chart.css';
+import ChartBar from './ChartBar';
+
+const Chart = props => {
+  return (
+    <div className="chart">
+      {props.dataPoints.map(dataPoint => (
+        <ChartBar
+          key={dataPoint.label}
+          value={dataPoint.value}
+          maxValue={null}
+          label={dataPoint.label}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default Chart;
