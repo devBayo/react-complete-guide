26faa64eb60f38d34a644e64e764554321d3ee13
hashvector <ibrahimabdulqoyyum@gmail.com>
2022-10-17 10:30:28 +0100

    modified expense form

--- src/components/NewExpense/ExpenseForm.jsx
+++ src/components/NewExpense/ExpenseForm.jsx
@@ -81,6 +81,9 @@ const ExpenseForm = props => {
       </div>
 
       <div className="new-expense__actions">
+        <button type="button" onClick={props.onCancel}>
+          Cancel
+        </button>
         <button type="submit">Add Expense</button>
       </div>
     </form>
