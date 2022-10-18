799b1360de175ba817ba0dca1f1443b7dfc5961c
hashvector <ibrahimabdulqoyyum@gmail.com>
2022-10-18 18:57:54 +0100

    setting dynamic inline styles

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -8,6 +8,7 @@ const CourseInput = props => {
   const [isValid, setIsValid] = useState(true);
 
   const goalInputChangeHandler = event => {
+    if (event.target.value.trim().length > 0) setIsValid(true);
     setEnteredValue(event.target.value);
   };
 
