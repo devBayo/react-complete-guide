a88a532f635fa888cd8ca3ac1fb48ac7566a7651
hashvector <ibrahimabdulqoyyum@gmail.com>
2022-10-18 23:06:25 +0100

    project setup

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -3,18 +3,19 @@ import React, { useState } from 'react';
 import Button from '../../UI/Button/Button';
 import styles from './CourseInput.module.css';
 
-const CourseInput = props => {
+const CourseInput = (props) => {
   const [enteredValue, setEnteredValue] = useState('');
   const [isValid, setIsValid] = useState(true);
 
-  const goalInputChangeHandler = event => {
-    if (event.target.value.trim().length > 0) setIsValid(true);
+  const goalInputChangeHandler = (event) => {
+    if (event.target.value.trim().length > 0) {
+      setIsValid(true);
+    }
     setEnteredValue(event.target.value);
   };
 
-  const formSubmitHandler = event => {
+  const formSubmitHandler = (event) => {
     event.preventDefault();
-
     if (enteredValue.trim().length === 0) {
       setIsValid(false);
       return;
@@ -25,7 +26,7 @@ const CourseInput = props => {
   return (
     <form onSubmit={formSubmitHandler}>
       <div
-        className={`${styles['form-control']} ${!isValid && styles['invalid']}`}
+        className={`${styles['form-control']} ${!isValid && styles.invalid}`}
       >
         <label>Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler} />
