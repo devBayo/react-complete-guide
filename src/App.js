c617f19f9e9c2ee1d1c815f2b53b483280a70faa
hashvector <ibrahimabdulqoyyum@gmail.com>
2022-10-19 10:38:46 +0100

    added core feat: new user can be added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { container } from './App.module.css';
 import Form from './components/Form/Form';
 import UserList from './components/User/UserList';
 
+const DUMMY_USERS = [
+  {
+    username: 'John',
+    age: 20,
+  },
+  {
+    username: 'Doe',
+    age: 30,
+  },
+];
+
 function App() {
+  const [users, setUsers] = useState(DUMMY_USERS);
+
+  const onAddUserHandler = newUser => {
+    setUsers(prevUsers => [newUser, ...prevUsers]);
+  };
+
   return (
     <div className={container}>
-      <Form />
-      <UserList />
+      <Form onAddUser={onAddUserHandler} />
+      <UserList users={users} />
     </div>
   );
 }
