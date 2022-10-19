2026eb305d2bc171ddf493671c427219fe4c3bfe
hashvector <ibrahimabdulqoyyum@gmail.com>
2022-10-19 10:38:41 +0100

    added core feat: new user can be added

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
 import styles from './Form.module.css';
 
-const Form = () => {
+const Form = props => {
+  const onSubmitHandler = event => {
+    event.preventDefault();
+    const username = event.target['0'].value;
+    const age = event.target['1'].value;
+
+    if (!username.length || !age.length) return;
+    const newUser = {
+      username,
+      age: +age,
+    };
+
+    event.target['0'].value = '';
+    event.target['1'].value = '';
+
+    props.onAddUser(newUser);
+  };
+
   return (
-    <form className={styles['form']}>
+    <form className={styles['form']} onSubmit={onSubmitHandler}>
       <div className={styles['form-group']}>
         <label htmlFor="name" className={styles['form-label']}>
           Username
