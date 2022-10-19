81d45cba8512163f9adcc49e6e670665b9f1f17a
hashvector <ibrahimabdulqoyyum@gmail.com>
2022-10-19 13:37:48 +0100

    react fragment

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -4,9 +4,9 @@ import Card from './Card';
 import Button from './Button';
 import classes from './ErrorModal.module.css';
 
-const ErrorModal = (props) => {
+const ErrorModal = props => {
   return (
-    <div>
+    <>
       <div className={classes.backdrop} onClick={props.onConfirm} />
       <Card className={classes.modal}>
         <header className={classes.header}>
@@ -19,7 +19,7 @@ const ErrorModal = (props) => {
           <Button onClick={props.onConfirm}>Okay</Button>
         </footer>
       </Card>
-    </div>
+    </>
   );
 };
 
