24d9634e0bd879b60c15fb941aa1d13acc50404b
hashvector <ibrahimabdulqoyyum@gmail.com>
2022-10-19 11:37:14 +0100

    added modal

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from './Form.module.css';
+import Modal from '../Modal/Modal';
 
 const Form = props => {
   const onSubmitHandler = event => {
@@ -20,23 +21,26 @@ const Form = props => {
   };
 
   return (
-    <form className={styles['form']} onSubmit={onSubmitHandler}>
-      <div className={styles['form-group']}>
-        <label htmlFor="name" className={styles['form-label']}>
-          Username
-        </label>
-        <input type="text" id="name" className={styles['form-input']} />
-      </div>
-      <div className={styles['form-group']}>
-        <label htmlFor="age" className={styles['form-label']}>
-          Age(Years)
-        </label>
-        <input type="number" id="age" className={styles['form-input']} />
-      </div>
-      <button type="submit" className={styles['form-button']}>
-        Add User
-      </button>
-    </form>
+    <div>
+      <form className={styles['form']} onSubmit={onSubmitHandler}>
+        <div className={styles['form-group']}>
+          <label htmlFor="name" className={styles['form-label']}>
+            Username
+          </label>
+          <input type="text" id="name" className={styles['form-input']} />
+        </div>
+        <div className={styles['form-group']}>
+          <label htmlFor="age" className={styles['form-label']}>
+            Age(Years)
+          </label>
+          <input type="number" id="age" className={styles['form-input']} />
+        </div>
+        <button type="submit" className={styles['form-button']}>
+          Add User
+        </button>
+      </form>
+      <Modal />
+    </div>
   );
 };
 
