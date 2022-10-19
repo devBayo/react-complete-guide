39855bdda72eb6d0d382605ece8e6cde190ebbea
hashvector <ibrahimabdulqoyyum@gmail.com>
2022-10-19 10:55:19 +0100

    modified

--- src/components/User/UserItem.jsx
+++ src/components/User/UserItem.jsx
@@ -3,7 +3,8 @@ import styles from './UserItem.module.css';
 const UserItem = props => {
   return (
     <div className={styles['user-item']}>
-      {props.user.username} {props.user.age}
+      {props.user.username} ({props.user.age}{' '}
+      {props.user.age > 1 ? 'years' : 'year'} old)
     </div>
   );
 };
