cf3c074aad33204992e9e79a5c68d602154ec78a
hashvector <ibrahimabdulqoyyum@gmail.com>
2022-10-19 10:32:32 +0100

    refactored UserList

--- src/components/User/UserList.jsx
+++ src/components/User/UserList.jsx
@@ -3,25 +3,10 @@ import React from 'react';
 import UserItem from './UserItem';
 import styles from './UserList.module.css';
 
-const userlist = [
-  {
-    username: 'John',
-    age: 20,
-  },
-  {
-    username: 'Doe',
-    age: 30,
-  },
-  {
-    username: 'John',
-    age: 20,
-  },
-];
-
-const UserList = () => {
+const UserList = props => {
   return (
     <div className={styles['user-list']}>
-      {userlist.map(user => (
+      {props.users.map(user => (
         <UserItem user={user} key={Math.random().toString()} />
       ))}
     </div>
