37660f013d48fa5fde2bf84f7cf7610d817ddf14
hashvector <ibrahimabdulqoyyum@gmail.com>
2022-10-19 09:19:53 +0100

    added container styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
+import { container } from './App.module.css';
 import Form from './components/Form/Form';
+import UserList from './components/User/UserList';
 
 function App() {
   return (
-    <div>
+    <div className={container}>
       <Form />
+      <UserList />
     </div>
   );
 }
