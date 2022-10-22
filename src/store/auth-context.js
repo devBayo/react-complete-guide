1039e40f4c6df419a43ba50a07a8cba6e5cdbb86
hashvector <ibrahimabdulqoyyum@gmail.com>
2022-10-22 09:43:49 +0100

    custom context provider component

--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -1,7 +1,39 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const AuthContext = React.createContext({
   isLoggedIn: false,
+  onLogout: () => {},
+  onLogin: () => {},
 });
+
+export const AuthContextProvider = props => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const storedUserLoggedInInfo = localStorage.getItem('isLoggedIn');
+
+    if (storedUserLoggedInInfo === '1') {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
+  const logoutHandler = () => {
+    localStorage.removeItem('isLoggedIn');
+    setIsLoggedIn(false);
+  };
+
+  const loginHandler = () => {
+    localStorage.setItem('isLoggedIn', '1');
+    setIsLoggedIn(true);
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{ isLoggedIn, onLogout: logoutHandler, onLogin: loginHandler }}
+    >
+      {props.children}
+    </AuthContext.Provider>
+  );
+};
 
 export default AuthContext;
