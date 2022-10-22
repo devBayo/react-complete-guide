79a18ae12bb5ebc52f1bcc29c15e6a010ca4c583
hashvector <ibrahimabdulqoyyum@gmail.com>
2022-10-22 09:14:31 +0100

    making context dynamic

--- src/App.js
+++ src/App.js
@@ -32,9 +32,10 @@ function App() {
     <AuthContext.Provider
       value={{
         isLoggedIn,
+        onLogout: logoutHandler,
       }}
     >
-      <MainHeader onLogout={logoutHandler} />
+      <MainHeader />
       <main>
         {!isLoggedIn && <Login onLogin={loginHandler} />}
         {isLoggedIn && <Home onLogout={logoutHandler} />}
