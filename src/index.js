15d923a2132e09717fb7d010ea433a1b7928237c
hashvector <ibrahimabdulqoyyum@gmail.com>
2022-10-22 22:25:30 +0100

    project setup

--- src/index.js
+++ src/index.js
@@ -1,13 +1,6 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
 
 import './index.css';
 import App from './App';
-import { AuthContextProvider } from './store/auth-context';
 
-ReactDOM.render(
-  <AuthContextProvider>
-    <App />
-  </AuthContextProvider>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
