46abc9e2a543fc99d4414502e3188ccedaf29aae
hashvector <ibrahimabdulqoyyum@gmail.com>
2022-10-22 09:43:46 +0100

    custom context provider component

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,8 +1,9 @@
-import React, { useState, useEffect, useReducer } from 'react';
+import React, { useState, useEffect, useReducer, useContext } from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
 import Button from '../UI/Button/Button';
+import AuthContext from '../../store/auth-context';
 
 const emailReducer = (state, action) => {
   if (action.type === 'USER_INPUT') {
@@ -76,9 +77,11 @@ const Login = props => {
     dispatchPassword({ type: 'INPUT_BLUR' });
   };
 
+  const context = useContext(AuthContext);
+
   const submitHandler = event => {
     event.preventDefault();
-    props.onLogin(emailState.value, passwordState.value);
+    context.onLogin(emailState.value, passwordState.value);
   };
 
   return (
