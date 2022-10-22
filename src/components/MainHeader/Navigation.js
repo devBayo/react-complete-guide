f3f9568008d654a2d6992caeb74102fb1271d35b
hashvector <ibrahimabdulqoyyum@gmail.com>
2022-10-22 09:14:34 +0100

    making context dynamic

--- src/components/MainHeader/Navigation.js
+++ src/components/MainHeader/Navigation.js
@@ -3,7 +3,7 @@ import AuthContext from '../../store/auth-context';
 
 import classes from './Navigation.module.css';
 
-const Navigation = props => {
+const Navigation = () => {
   const context = useContext(AuthContext);
   return (
     <nav className={classes.nav}>
@@ -20,7 +20,7 @@ const Navigation = props => {
         )}
         {context.isLoggedIn && (
           <li>
-            <button onClick={props.onLogout}>Logout</button>
+            <button onClick={context.onLogout}>Logout</button>
           </li>
         )}
       </ul>
