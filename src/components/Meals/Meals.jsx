0bdc1c1ead15cccccd6a230241ba8cfc976b8fd2
hashvector <ibrahimabdulqoyyum@gmail.com>
2022-10-25 22:38:16 +0100

    added meals

---
+++ src/components/Meals/Meals.jsx
@@ -0,0 +1,13 @@
+import AvalableMeals from './AvailableMeals';
+import MealsSummary from './MealsSummary';
+
+const Meals = () => {
+  return (
+    <>
+      <MealsSummary />
+      <AvalableMeals />
+    </>
+  );
+};
+
+export default Meals;
