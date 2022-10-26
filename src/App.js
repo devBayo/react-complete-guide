782ab851e7210d9fdc1fb5875df899d083eae9ca
hashvector <ibrahimabdulqoyyum@gmail.com>
2022-10-26 09:13:08 +0100

    managing cart and modal state

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
+import { useState } from 'react';
 import Cart from './components/Cart/Cart';
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 
 function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  const showCartHandler = () => {
+    setCartIsShown(true);
+  };
+
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+  };
+
   return (
     <>
-      <Cart />
-      <Header />
+      {cartIsShown && <Cart onCloseCart={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
