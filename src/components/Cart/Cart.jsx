d39789e9aaefb6d90b0112b0f65e4182cb6317b4
hashvector <ibrahimabdulqoyyum@gmail.com>
2022-10-26 08:57:14 +0100

    added modal

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,15 +1,16 @@
+import Modal from '../UI/Modal';
 import classes from './Cart.module.css';
 
 const Cart = props => {
   const cartItems = (
-    <ul className={classes['cart-item']}>
+    <ul className={classes['cart-items']}>
       {[{ id: 'c1', name: 'Suchi', amount: 2, price: 12.99 }].map(item => (
         <li>{item.name}</li>
       ))}
     </ul>
   );
   return (
-    <div>
+    <Modal>
       {cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
@@ -19,7 +20,7 @@ const Cart = props => {
         <button className={classes['button--alt']}>Close</button>
         <button className={classes['button']}>Order</button>
       </div>
-    </div>
+    </Modal>
   );
 };
 export default Cart;
