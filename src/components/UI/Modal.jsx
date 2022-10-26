1fc489a7515532996ef0004de4f84b55ba7a09da
hashvector <ibrahimabdulqoyyum@gmail.com>
2022-10-26 09:13:22 +0100

    managing cart and modal state

--- src/components/UI/Modal.jsx
+++ src/components/UI/Modal.jsx
@@ -2,7 +2,7 @@ import { createPortal } from 'react-dom';
 import classes from './Modal.module.css';
 
 const Backdrop = props => {
-  return <div className={classes.backdrop}></div>;
+  return <div className={classes.backdrop} onClick={props.onClick}></div>;
 };
 const ModalOverlay = props => {
   return (
@@ -17,7 +17,7 @@ const portalElement = document.getElementById('overlays');
 const Modal = props => {
   return (
     <>
-      {createPortal(<Backdrop />, portalElement)}
+      {createPortal(<Backdrop onClick={props.onClick} />, portalElement)}
       {createPortal(
         <ModalOverlay>{props.children}</ModalOverlay>,
         portalElement
