9641cc2123166ce37cc56142c220fc78b003d420
hashvector <ibrahimabdulqoyyum@gmail.com>
2022-10-26 09:55:18 +0100

    feat: using cart context to show total items

--- src/components/Layout/HeaderCartButton.jsx
+++ src/components/Layout/HeaderCartButton.jsx
@@ -1,14 +1,22 @@
+import { useContext } from 'react';
+import CartContext from '../../store/cart-context';
 import CartIcon from '../Cart/CartIcon';
 import classes from './HeaderCartButton.module.css';
 
 const HeaderCartButton = props => {
+  const cartContext = useContext(CartContext);
+  const totalCartItems = cartContext.items.reduce(
+    (curItem, item) => curItem + item.amount,
+    0
+  );
+
   return (
     <button className={classes.button} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={classes.badge}>3</span>
+      <span className={classes.badge}>{totalCartItems}</span>
     </button>
   );
 };
