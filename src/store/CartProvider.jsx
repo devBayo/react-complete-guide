1735b48491098ba56eabb4894f786210f52b4883
hashvector <ibrahimabdulqoyyum@gmail.com>
2022-10-27 02:56:54 +0100

    passed amount(count) through fowardRef

--- src/store/CartProvider.jsx
+++ src/store/CartProvider.jsx
@@ -1,12 +1,42 @@
+import { useReducer } from 'react';
 import CartContext from './cart-context';
 
+const defaultCartState = {
+  items: [],
+  totalAmount: 0,
+};
+
+const cartReducer = (state, action) => {
+  if (action.type === 'ADD') {
+    const updatedItems = state.items.concat(action.item);
+    const updatedTotalAmount =
+      state.totalAmount + action.item.price * action.item.amount;
+
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount,
+    };
+  }
+
+  return defaultCartState;
+};
 const CartProvider = props => {
-  const addItemTOCartHandler = item => {};
-  const removeItemFromCartHandler = id => {};
+  const [cartState, dispatchCartState] = useReducer(
+    cartReducer,
+    defaultCartState
+  );
+
+  const addItemTOCartHandler = item => {
+    dispatchCartState({ type: 'ADD', item });
+  };
+
+  const removeItemFromCartHandler = id => {
+    dispatchCartState({ type: 'REMOVE', id });
+  };
 
   const cartContext = {
-    items: [],
-    totalAmount: 0,
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
     addItem: addItemTOCartHandler,
     removeItem: removeItemFromCartHandler,
   };
