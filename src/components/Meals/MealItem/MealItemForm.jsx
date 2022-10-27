b01eadce8e35b9ed34e29c6679afade877a91cfb
hashvector <ibrahimabdulqoyyum@gmail.com>
2022-10-27 04:56:28 +0100

    fixed form validation

--- src/components/Meals/MealItem/MealItemForm.jsx
+++ src/components/Meals/MealItem/MealItemForm.jsx
@@ -1,25 +1,14 @@
-import { useRef, useState } from 'react';
+import { useRef } from 'react';
 import Input from '../../UI/Input';
 import classes from './MealItemForm.module.css';
 
 const MealItemForm = props => {
-  const [amountIsValid, setAmountIsValid] = useState(true);
-
   const amountInputRef = useRef();
 
   const submitHandler = event => {
     event.preventDefault();
     const enteredAmount = amountInputRef.current.value;
     const enteredAmountNumber = +enteredAmount;
-
-    if (
-      enteredAmount.trim().length === 0 ||
-      enteredAmountNumber < 1 ||
-      enteredAmountNumber > 5
-    ) {
-      setAmountIsValid(false);
-      return;
-    }
 
     props.onAddToCart(enteredAmountNumber);
   };
@@ -35,11 +24,11 @@ const MealItemForm = props => {
           min: '1',
           max: '5',
           step: '1',
+          required: 'required',
           defaultValue: '1',
         }}
       />
       <button>+ Add</button>
-      {!amountIsValid && <p>Please enter a valid amount (1-5)</p>}
     </form>
   );
 };
