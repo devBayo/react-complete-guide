fb67dba5bf8a48593175b59a4eda887074f00acf
hashvector <ibrahimabdulqoyyum@gmail.com>
2022-10-27 03:40:42 +0100

    outputting cart item

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,11 +1,27 @@
 import Modal from '../UI/Modal';
 import classes from './Cart.module.css';
+import { useContext } from 'react';
+import CartContext from '../../store/cart-context';
+import CartItem from './CartItem';
 
 const Cart = props => {
+  const cartContext = useContext(CartContext);
+  const hasItems = cartContext.items.length > 0;
+
+  const cartItemRemoveHandler = id => {};
+  const cartItemAddHandler = item => {};
+
   const cartItems = (
     <ul className={classes['cart-items']}>
-      {[{ id: 'c1', name: 'Suchi', amount: 2, price: 12.99 }].map(item => (
-        <li key={item.id}>{item.name}</li>
+      {cartContext.items.map(item => (
+        <CartItem
+          key={item.id}
+          name={item.name}
+          amount={item.amount}
+          price={item.price}
+          onRemove={cartItemRemoveHandler.bind(null, item.id)}
+          onAdd={cartItemAddHandler.bind(null, item)}
+        />
       ))}
     </ul>
   );
@@ -14,13 +30,13 @@ const Cart = props => {
       {cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
-        <span>35.62</span>
+        <span>${cartContext.totalAmount.toFixed(2)}</span>
       </div>
       <div className={classes.actions}>
         <button className={classes['button--alt']} onClick={props.onCloseCart}>
           Close
         </button>
-        <button className={classes['button']}>Order</button>
+        {hasItems && <button className={classes['button']}>Order</button>}
       </div>
     </Modal>
   );
---
+++ src/components/Cart/CartItem.jsx
@@ -0,0 +1,21 @@
+import classes from './CartItem.module.css';
+
+const CartItem = props => {
+  return (
+    <li className={classes['cart-item']}>
+      <div>
+        <h2>{props.name}</h2>
+        <div className={classes.summary}>
+          <span className={classes.price}>${props.price.toFixed(2)}</span>
+          <span className={classes.amount}>x {props.amount}</span>
+        </div>
+      </div>
+      <div className={classes.actions}>
+        <button onClick={props.onRemove}>−</button>
+        <button onClick={props.onAdd}>+</button>
+      </div>
+    </li>
+  );
+};
+
+export default CartItem;
