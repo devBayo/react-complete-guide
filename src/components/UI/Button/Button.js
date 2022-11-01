182d8c268886ce676b6691695179f6bad80e767b
hashvector <ibrahimabdulqoyyum@gmail.com>
2022-11-01 19:56:18 +0100

    intro to React memo

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -2,7 +2,8 @@ import React from 'react';
 
 import classes from './Button.module.css';
 
-const Button = (props) => {
+const Button = props => {
+  console.log('Button running');
   return (
     <button
       type={props.type || 'button'}
@@ -15,4 +16,4 @@ const Button = (props) => {
   );
 };
 
-export default Button;
+export default React.memo(Button);
