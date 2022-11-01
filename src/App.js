cf32b5662c7a24ebb606e9f4b449af12f208f198
hashvector <ibrahimabdulqoyyum@gmail.com>
2022-11-01 20:18:45 +0100

    useCallback and its dependencies

--- src/App.js
+++ src/App.js
@@ -6,17 +6,23 @@ import Button from './components/UI/Button/Button';
 
 function App() {
   const [showParagrah, setShowParagrah] = useState(false);
+  const [allowParagraph, setAllowParagraph] = useState(false);
 
   console.log('App running');
 
   const toggleParagraphHandler = useCallback(() => {
-    setShowParagrah(prevValue => !prevValue);
-  }, []);
+    if (allowParagraph) setShowParagrah(prevValue => !prevValue);
+  }, [allowParagraph]);
+
+  const allowParagraphHandler = () => {
+    setAllowParagraph(true);
+  };
 
   return (
     <div className="app">
       <h1>Hi there!</h1>
       {<DemoOutput show={false} />}
+      <Button onClick={allowParagraphHandler}>Allow Paragraph</Button>
       <Button onClick={toggleParagraphHandler}>Toggle Paragraph</Button>
     </div>
   );
