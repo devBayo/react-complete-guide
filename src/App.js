a3502337b01d9ebe1101f93b6391ddd5897ce5bc
hashvector <ibrahimabdulqoyyum@gmail.com>
2022-11-01 18:12:41 +0100

    components updates in action

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './App.css';
+import Button from './components/UI/Button/Button';
 
 function App() {
+  const [showParagrah, setShowParagrah] = useState(false);
+
+  console.log('App running');
+
+  const toggleParagraphHandler = () => {
+    setShowParagrah(prevValue => !prevValue);
+  };
+
   return (
     <div className="app">
       <h1>Hi there!</h1>
+      {showParagrah && <p>This is new</p>}
+      <Button onClick={toggleParagraphHandler}>Toggle Paragraph</Button>
     </div>
   );
 }
