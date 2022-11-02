8b0e23838f8ba3b69c3d0c077957d34ee72a2d5b
hashvector <ibrahimabdulqoyyum@gmail.com>
2022-11-02 09:39:21 +0100

    project setup

--- src/App.js
+++ src/App.js
@@ -1,29 +1,9 @@
-import React, { useCallback, useState } from 'react';
-
-import './App.css';
-import DemoOutput from './components/Demo/DemoOutput';
-import Button from './components/UI/Button/Button';
+import Users from './components/Users';
 
 function App() {
-  const [showParagrah, setShowParagrah] = useState(false);
-  const [allowParagraph, setAllowParagraph] = useState(false);
-
-  console.log('App running');
-
-  const toggleParagraphHandler = useCallback(() => {
-    if (allowParagraph) setShowParagrah(prevValue => !prevValue);
-  }, [allowParagraph]);
-
-  const allowParagraphHandler = () => {
-    setAllowParagraph(true);
-  };
-
   return (
-    <div className="app">
-      <h1>Hi there!</h1>
-      {<DemoOutput show={showParagrah} />}
-      <Button onClick={allowParagraphHandler}>Allow Paragraph</Button>
-      <Button onClick={toggleParagraphHandler}>Toggle Paragraph</Button>
+    <div>
+      <Users />
     </div>
   );
 }
