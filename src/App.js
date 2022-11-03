8109bf1f9e3d6834cdd1b72910a78fbfffea1b56
hashvector <ibrahimabdulqoyyum@gmail.com>
2022-11-03 10:54:58 +0100

    project setup

--- src/App.js
+++ src/App.js
@@ -1,21 +1,33 @@
-import UserFinder from './components/UserFinder';
-import UsersContext from './store/users-context';
+import React from 'react';
 
-const DUMMY_USERS = [
-  { id: 'u1', name: 'Max' },
-  { id: 'u2', name: 'Manuel' },
-  { id: 'u3', name: 'Julie' },
-];
+import MoviesList from './components/MoviesList';
+import './App.css';
 
 function App() {
-  const usersContext = {
-    users: DUMMY_USERS,
-  };
+  const dummyMovies = [
+    {
+      id: 1,
+      title: 'Some Dummy Movie',
+      openingText: 'This is the opening text of the movie',
+      releaseDate: '2021-05-18',
+    },
+    {
+      id: 2,
+      title: 'Some Dummy Movie 2',
+      openingText: 'This is the second opening text of the movie',
+      releaseDate: '2021-05-19',
+    },
+  ];
 
   return (
-    <UsersContext.Provider value={usersContext}>
-      <UserFinder />
-    </UsersContext.Provider>
+    <React.Fragment>
+      <section>
+        <button>Fetch Movies</button>
+      </section>
+      <section>
+        <MoviesList movies={dummyMovies} />
+      </section>
+    </React.Fragment>
   );
 }
 
