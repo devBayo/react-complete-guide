4cbf75b4b00a83d90d36b36cea992add0999e236
hashvector <ibrahimabdulqoyyum@gmail.com>
2022-11-03 22:44:21 +0100

    project setup

--- src/App.js
+++ src/App.js
@@ -1,46 +1,10 @@
-import React, { useEffect, useState } from 'react';
-
-import Tasks from './components/Tasks/Tasks';
-import NewTask from './components/NewTask/NewTask';
-import useHttp from './hooks/use-http';
+import SimpleInput from './components/SimpleInput';
 
 function App() {
-  const [tasks, setTasks] = useState([]);
-
-  const transformedTasks = taskData => {
-    const loadedTasks = [];
-
-    for (const taskKey in taskData) {
-      loadedTasks.push({ id: taskKey, text: taskData[taskKey].text });
-    }
-
-    setTasks(loadedTasks);
-  };
-
-  const { error, isLoading, sendRequest: fetchTasks } = useHttp();
-
-  useEffect(() => {
-    fetchTasks(
-      'https://react-guide-http-fb145-default-rtdb.firebaseio.com/tasks.json',
-      null,
-      transformedTasks
-    );
-  }, [fetchTasks]);
-
-  const taskAddHandler = task => {
-    setTasks(prevTasks => prevTasks.concat(task));
-  };
-
   return (
-    <React.Fragment>
-      <NewTask onAddTask={taskAddHandler} />
-      <Tasks
-        items={tasks}
-        loading={isLoading}
-        error={error}
-        onFetch={fetchTasks}
-      />
-    </React.Fragment>
+    <div className="app">
+      <SimpleInput />
+    </div>
   );
 }
 
