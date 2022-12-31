b4eb6fb4c48e201ec795b0717a0f2a06af6e6574
hashvector <ibrahimabdulqoyyum@gmail.com>
2022-12-31 14:55:23 +0100

    intro to redux toolkit

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,31 @@
-import { createStore } from 'redux';
+import { legacy_createStore as createStore } from 'redux';
+
+import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = { counter: 0, showCounter: true };
+
+createSlice({
+  name: 'counter',
+  initialState,
+  reducers: {
+    increment(state) {
+      state.counter++;
+    },
+
+    decrement(state) {
+      state.counter--;
+    },
+
+    increase(state, action) {
+      state.counter += action.amount;
+    },
+
+    toggle(state) {
+      state.showCounter = !state.showCounter;
+    },
+  },
+});
+
 const counterReducer = (state = initialState, action) => {
   if (action.type === 'increment') {
     console.log(state);
