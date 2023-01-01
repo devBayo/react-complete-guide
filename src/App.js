4e5c7dc6180c68268cfa86df51f77d2d2392a35e
hashvector <ibrahimabdulqoyyum@gmail.com>
2023-01-01 09:48:25 +0100

    project setup

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
-import React, { Fragment } from 'react';
-import Counter from './components/Counter';
-import Header from './components/Header';
-import Auth from './components/Auth';
-import UserProfile from './components/UserProfile';
-import { useSelector } from 'react-redux';
+import Cart from './components/Cart/Cart';
+import Layout from './components/Layout/Layout';
+import Products from './components/Shop/Products';
 
 function App() {
-  const isAuth = useSelector(state => state.auth.isAuthenticated);
   return (
-    <Fragment>
-      <Header />
-      {isAuth ? <UserProfile /> : <Auth />}
-      <Counter />
-    </Fragment>
+    <Layout>
+      <Cart />
+      <Products />
+    </Layout>
   );
 }
 
--- src/store/index.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import authReducer from './auth';
-import counterReducer from './counter';
-
-const store = configureStore({
-  reducer: { counter: counterReducer, auth: authReducer },
-});
-
-export default store;
