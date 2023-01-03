c09d13ead01b8568e9a2d6043c97bc88b3805df6
hashvector <ibrahimabdulqoyyum@gmail.com>
2023-01-03 04:51:52 +0100

    added header, container

--- src/App.jsx
+++ src/App.jsx
@@ -3,26 +3,29 @@ import { Redirect, Route, Switch } from 'react-router-dom';
 import Quotes from './pages/Quotes';
 import QuoteDetail from './pages/QuoteDetail';
 import NewQuote from './pages/NewQuote';
+import Layout from './components/layout/Layout';
 
 function App() {
   return (
-    <Switch>
-      <Route path="/" exact>
-        <Redirect to="/quotes" />
-      </Route>
+    <Layout>
+      <Switch>
+        <Route path="/" exact>
+          <Redirect to="/quotes" />
+        </Route>
 
-      <Route path="/quotes" exact>
-        <Quotes />
-      </Route>
+        <Route path="/quotes" exact>
+          <Quotes />
+        </Route>
 
-      <Route path="/quotes/:quoteId">
-        <QuoteDetail />
-      </Route>
+        <Route path="/quotes/:quoteId">
+          <QuoteDetail />
+        </Route>
 
-      <Route path="/new-quote">
-        <NewQuote />
-      </Route>
-    </Switch>
+        <Route path="/new-quote">
+          <NewQuote />
+        </Route>
+      </Switch>
+    </Layout>
   );
 }
 
