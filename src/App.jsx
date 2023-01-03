5ccab7fb8a5548d90a2d321e226c4b39402b85e0
hashvector <ibrahimabdulqoyyum@gmail.com>
2023-01-03 02:43:26 +0100

    using an action creator thunk.

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 import Notification from './components/UI/Notification';
-import { cartActions } from './store/cart';
+import { sendCartData } from './store/cart';
 
 let isInitial = true;
 
@@ -15,49 +15,12 @@ function App() {
   const { notification } = useSelector(state => state);
 
   useEffect(() => {
-    const sendData = async () => {
-      try {
-        dispatch(
-          cartActions.showNotification({
-            status: 'pending',
-            title: 'Sending...',
-            message: 'Sending cart data!',
-          })
-        );
-
-        const response = await fetch(
-          'https://react-guide-http-fb145-default-rtdb.firebaseio.com/cart.json',
-          {
-            method: 'PUT',
-            body: JSON.stringify(cart),
-          }
-        );
-
-        if (!response.ok) throw new Error();
-
-        dispatch(
-          cartActions.showNotification({
-            status: 'success',
-            title: 'Success!',
-            message: 'Sent cart data successfully!',
-          })
-        );
-      } catch (error) {
-        dispatch(
-          cartActions.showNotification({
-            status: 'error',
-            title: 'Error!',
-            message: 'Sending cart data failed!',
-          })
-        );
-      }
-    };
-
     if (isInitial) {
       isInitial = false;
       return;
     }
-    sendData();
+
+    dispatch(sendCartData(cart));
   }, [cart, dispatch]);
 
   return (
