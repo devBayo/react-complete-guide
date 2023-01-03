a41a1e7fceafe0eab453a6964ca5dda85643814e
hashvector <ibrahimabdulqoyyum@gmail.com>
2023-01-03 08:18:03 +0100

    intro to navigation

--- src/pages/NewQuote.jsx
+++ src/pages/NewQuote.jsx
@@ -1,8 +1,13 @@
 import QuoteForm from '../components/quotes/QuoteForm';
+import { useHistory } from 'react-router-dom';
 
 const NewQuote = () => {
+  const history = useHistory();
+
   const addQuoteHandler = quoteData => {
     console.log(quoteData);
+
+    history.push('/quotes');
   };
 
   return <QuoteForm onAddQuote={addQuoteHandler} />;
