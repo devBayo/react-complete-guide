673046c2decc6c2901d601290c4ac6c2d412f0d0
hashvector <ibrahimabdulqoyyum@gmail.com>
2023-01-03 03:44:39 +0100

    chore: code clean up

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { Route } from 'react-router-dom';
-import Products from './components/Products';
-import Welcome from './components/Welcome';
+import MainHeader from './components/MainHeader';
+import Products from './pages/Products';
+import Welcome from './pages/Welcome';
 
 function App() {
   return (
     <div>
+      <MainHeader />
       <Route path="/welcome">
         <Welcome />
       </Route>
