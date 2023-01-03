e9662ae80aa58f96c827ad01b8183059f9478102
hashvector <ibrahimabdulqoyyum@gmail.com>
2023-01-03 02:43:29 +0100

    using an action creator thunk.

--- src/store/cart.js
+++ src/store/cart.js
@@ -49,5 +49,45 @@ const cartSlice = createSlice({
   },
 });
 
+export const sendCartData = cart => {
+  return async dispatch => {
+    try {
+      dispatch(
+        cartActions.showNotification({
+          status: 'pending',
+          title: 'Sending...',
+          message: 'Sending cart data!',
+        })
+      );
+
+      const response = await fetch(
+        'https://react-guide-http-fb145-default-rtdb.firebaseio.com/cart.json',
+        {
+          method: 'PUT',
+          body: JSON.stringify(cart),
+        }
+      );
+
+      if (!response.ok) throw new Error('Sending cart data failed!');
+
+      dispatch(
+        cartActions.showNotification({
+          status: 'success',
+          title: 'Success!',
+          message: 'Sent cart data successfully!',
+        })
+      );
+    } catch (error) {
+      dispatch(
+        cartActions.showNotification({
+          status: 'error',
+          title: 'Error!',
+          message: 'Sending cart data failed!',
+        })
+      );
+    }
+  };
+};
+
 export const cartActions = cartSlice.actions;
 export default cartSlice.reducer;
