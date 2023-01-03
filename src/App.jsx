8f5fbea0968cbf837f70b34d6c376bff457789b1
hashvector <ibrahimabdulqoyyum@gmail.com>
2023-01-03 04:01:29 +0100

    using switch and exact

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import MainHeader from './components/MainHeader';
 import ProductDetail from './components/ProductDetail';
 import Products from './pages/Products';
@@ -9,15 +9,17 @@ function App() {
     <div>
       <MainHeader />
       <main>
-        <Route path="/welcome">
-          <Welcome />
-        </Route>
-        <Route path="/products">
-          <Products />
-        </Route>
-        <Route path="/product-detail/:productId">
-          <ProductDetail />
-        </Route>
+        <Switch>
+          <Route path="/welcome">
+            <Welcome />
+          </Route>
+          <Route path="/products" exact>
+            <Products />
+          </Route>
+          <Route path="/products/:productId">
+            <ProductDetail />
+          </Route>
+        </Switch>
       </main>
     </div>
   );
