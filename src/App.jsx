5d3188e1c67ead2d0ff6d2b496ec06e3bfa484c6
hashvector <ibrahimabdulqoyyum@gmail.com>
2023-01-03 03:23:42 +0100

    project setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,8 @@
-import { Fragment, useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import Cart from './components/Cart/Cart';
-import Layout from './components/Layout/Layout';
-import Products from './components/Shop/Products';
-import Notification from './components/UI/Notification';
-import { sendCartData, FetchCartData } from './store/cart';
-
-let isInitial = true;
-
 function App() {
-  const dispatch = useDispatch();
-  const { cartIsOpen, cart, notification, isChanged } = useSelector(
-    state => state
-  );
-
-  useEffect(() => {
-    dispatch(FetchCartData());
-  }, [dispatch]);
-
-  useEffect(() => {
-    if (isInitial) {
-      isInitial = false;
-      return;
-    }
-
-    if (isChanged) {
-      dispatch(sendCartData(cart));
-    }
-  }, [cart, dispatch, isChanged]);
-
   return (
-    <Fragment>
-      {notification && (
-        <Notification
-          title={notification.title}
-          message={notification.message}
-          status={notification.status}
-        />
-      )}
-      <Layout>
-        {cartIsOpen && <Cart />}
-        <Products />
-      </Layout>
-    </Fragment>
+    <div>
+      <h2>Let's get started!</h2>
+    </div>
   );
 }
 
