ce7f9b95d1b1cfe15abe417d2e17aed76a4f2774
hashvector <ibrahimabdulqoyyum@gmail.com>
2023-01-03 04:01:32 +0100

    using switch and exact

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -6,9 +6,15 @@ const Products = () => {
       <h1>The Products Page</h1>
 
       <ul>
-        {/* <li><Link to={}>A Book</Link></li> */}
-        {/* <li><Link to={}>A Carpet</Link></li> */}
-        {/* <li><Link to={}>A Course</Link></li> */}
+        <li>
+          <Link to="/products/p1">A Book</Link>
+        </li>
+        <li>
+          <Link to="/products/p2">A Carpet</Link>
+        </li>
+        <li>
+          <Link to="/products/p3">A Course</Link>
+        </li>
       </ul>
     </section>
   );
