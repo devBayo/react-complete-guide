0dfe557e61c7ed37d6e84f3ad8cc1b5d1f693aaa
hashvector <ibrahimabdulqoyyum@gmail.com>
2023-01-03 03:55:06 +0100

    extracting route params

--- src/components/ProductDetail.jsx
+++ src/components/ProductDetail.jsx
@@ -1,7 +1,13 @@
+import { useParams } from 'react-router';
+
 const ProductDetail = () => {
+  const params = useParams();
+  console.log(params);
+
   return (
     <section>
       <h1>Product Detail</h1>
+      <p>{params.productId}</p>
     </section>
   );
 };
