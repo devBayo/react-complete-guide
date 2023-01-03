315d65382ec8f8db3ec6b572da3158457db540d0
hashvector <ibrahimabdulqoyyum@gmail.com>
2023-01-03 04:04:50 +0100

    intro to nested route

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -1,6 +1,14 @@
+import { Route } from 'react-router';
+
 const Welcome = () => {
   return (
+    <section>
       <h1>The Welcome Page</h1>
+
+      <Route path="/welcome/new-user">
+        <p>Welcome new user</p>
+      </Route>
+    </section>
   );
 };
 
