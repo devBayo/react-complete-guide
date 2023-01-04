eedfa69c1fac000c10aa8b77b48433fe78b7d889
hashvector <ibrahimabdulqoyyum@gmail.com>
2023-01-04 04:44:44 +0100

    migrated from react-router-v5 to v6

--- src/components/quotes/QuoteList.jsx
+++ src/components/quotes/QuoteList.jsx
@@ -1,5 +1,5 @@
 import { Fragment } from 'react';
-import { useLocation, useHistory } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 import QuoteItem from './QuoteItem';
 import classes from './QuoteList.module.css';
@@ -15,7 +15,7 @@ const sortQuotes = (quotes, ascending) => {
 };
 
 const QuoteList = props => {
-  const history = useHistory();
+  const navigate = useNavigate();
   const location = useLocation();
 
   const queryParams = new URLSearchParams(location.search);
@@ -24,7 +24,7 @@ const QuoteList = props => {
   const sortedQuotes = sortQuotes(props.quotes, isSortingAscending);
 
   const changeSortingHandler = () => {
-    history.push({
+    navigate({
       pathname: location.pathname,
       search: `sort=${isSortingAscending ? 'desc' : 'asc'}`,
     });
