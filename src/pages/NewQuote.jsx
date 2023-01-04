dae635aa375b0a00defe2e52a983139b380ec8b4
hashvector <ibrahimabdulqoyyum@gmail.com>
2023-01-04 04:44:47 +0100

    migrated from react-router-v5 to v6

--- src/pages/NewQuote.jsx
+++ src/pages/NewQuote.jsx
@@ -1,18 +1,18 @@
 import QuoteForm from '../components/quotes/QuoteForm';
-import { useHistory } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import useHttp from '../hooks/use-http';
 import { addQuote } from '../lib/api';
 import { useEffect } from 'react';
 
 const NewQuote = () => {
   const { sendRequest, status } = useHttp(addQuote);
-  const history = useHistory();
+  const navigate = useNavigate();
 
   useEffect(() => {
     if (status === 'completed') {
-      history.push('/quotes');
+      navigate('/quotes');
     }
-  }, [status, history]);
+  }, [status, navigate]);
 
   const addQuoteHandler = quoteData => {
     sendRequest(quoteData);
