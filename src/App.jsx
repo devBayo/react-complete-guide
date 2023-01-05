942e1264bd8f5a1e71f4d715b18040263d093c26
hashvector <ibrahimabdulqoyyum@gmail.com>
2023-01-05 03:03:33 +0100

    added lazy loading.

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,29 @@
+import React, { Suspense } from 'react';
 import { Navigate, Route, Routes } from 'react-router-dom';
 
 import Quotes from './pages/Quotes';
-import QuoteDetail from './pages/QuoteDetail';
-import NewQuote from './pages/NewQuote';
 import Layout from './components/layout/Layout';
-import NotFound from './pages/NotFound';
-import Comments from './components/comments/Comments';
+import LoadingSpinner from './components/UI/LoadingSpinner';
+
+const NewQuote = React.lazy(() => import('./pages/NewQuote'));
+const QuoteDetail = React.lazy(() => import('./pages/QuoteDetail'));
+const NotFound = React.lazy(() => import('./pages/NotFound'));
+const Comments = React.lazy(() => import('./components/comments/Comments'));
 
 function App() {
   return (
     <Layout>
-      <Routes>
-        <Route path="/" element={<Navigate replace to="/quotes" />} />
-        <Route path="/quotes" element={<Quotes />} />
-        <Route path="/quotes/:quoteId/*" element={<QuoteDetail />}>
-          <Route path="comments" element={<Comments />} />
-        </Route>
-        <Route path="/new-quote" element={<NewQuote />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+      <Suspense fallback={<LoadingSpinner />}>
+        <Routes>
+          <Route path="/" element={<Navigate replace to="/quotes" />} />
+          <Route path="/quotes" element={<Quotes />} />
+          <Route path="/quotes/:quoteId/*" element={<QuoteDetail />}>
+            <Route path="comments" element={<Comments />} />
+          </Route>
+          <Route path="/new-quote" element={<NewQuote />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Suspense>
     </Layout>
   );
 }
